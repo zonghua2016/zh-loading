35bde98507a4a99e7451dcb7a22a3924dde508f3
zonghua2016 <2280314971@qq.com>
2019-11-05 10:38:13 +0800

    修改readme文档

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,15 @@
   <div>
     <ZhLoading :size="80" :cicleNum="4" :borderWidth="3" />
     <Typing>
-      <div>
-        <h2>
-          <p>
-            <span>中文测试chinese test</span>
-          </p>
-        </h2>
-      </div>
+     中文测试chinese test
     </Typing>
     <!-- <Typing>asf顺丰到付sfs</Typing> -->
   </div>
 </template>
 
 <script>
-import ZhLoading from './components/ZhLoading'
-import Typing from './components/Typing'
+import ZhLoading from './components/loading/ZhLoading'
+import Typing from './components/typing/Typing'
 export default {
     data(){
       return {
